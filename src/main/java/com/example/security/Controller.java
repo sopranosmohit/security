88bd0665845ad706dce3379661baf3a29eaaf2f6
Mohit <sopranosmohit@gmail.com>
2019-12-28 00:52:10 +0530

    Initial commit

---
+++ src/main/java/com/example/security/Controller.java
@@ -0,0 +1,27 @@
+package com.example.security;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String getME(){
+
+        return "<h1>Hello Peopleeesss</h1>";
+    }
+
+    @RequestMapping("/user")
+    public String user(){
+
+        return "<h1>Hello Guysssssss</h1>";
+    }
+    @RequestMapping("/admin")
+    public String admin(){
+
+        return "<h1>Hello Guyssssss</h1>";
+    }
+
+}
